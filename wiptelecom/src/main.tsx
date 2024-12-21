af804201a5cb13c006150bc637d170fbe88615cb
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2024-12-21 09:36:33 -0300

    Alterando diretorio dos Contratos

--- wiptelecom/src/main.tsx
+++ wiptelecom/src/main.tsx
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter, Routes, Route } from "react-router";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { About } from "./pages/About";
 import { Contact } from "./pages/Contact";
 import { Contract } from "./pages/Contract";
