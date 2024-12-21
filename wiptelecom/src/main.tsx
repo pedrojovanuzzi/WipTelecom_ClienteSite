a14080940952fe86b87730d3410d5360cc410b21
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2024-12-21 09:34:39 -0300

    Alterando diretorio dos Contratos

--- wiptelecom/src/main.tsx
+++ wiptelecom/src/main.tsx
@@ -1,34 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router";
 
-import { About } from './pages/About';
-import { Contact } from './pages/Contact';
-import { Contract } from './pages/Contract';
-import NotFound from './pages/NotFound';
-import PoliticsEmail from './pages/PoliticsEmail';
-import PoliticsApp from './pages/PoliticsApp';
-
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
+import { Contract } from "./pages/Contract";
+import NotFound from "./pages/NotFound";
+import PoliticsEmail from "./pages/PoliticsEmail";
+import PoliticsApp from "./pages/PoliticsApp";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
-
 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route path="*" element={<NotFound />} />
         <Route path="/" element={<App />} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/contact" element={<Contact/>} />
-        <Route path="/contrato" element={<Contract/>} />
-        <Route path="/politica-de-privacidade-email" element={<PoliticsEmail/>} />
-        <Route path="/politica-de-privacidade-app" element={<PoliticsApp/>} />
-        <Route path="/politica" element={<PoliticsApp/>} />
-        <Route path='*' element={<NotFound />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/contrato" element={<Contract />} />
+        <Route
+          path="/politica-de-privacidade-email"
+          element={<PoliticsEmail />}
+        />
+        <Route path="/politica-de-privacidade-app" element={<PoliticsApp />} />
+        <Route path="/politica" element={<PoliticsApp />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
